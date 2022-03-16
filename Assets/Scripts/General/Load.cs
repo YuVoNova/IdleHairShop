905cb5d0139e770d4308e4d43c704a3566fc8af4
Yusuf Metindoğan <yusufmdogan@gmail.com>
2022-03-16 14:36:00 +0300

    Initialized Essentials

---
+++ Assets/Scripts/General/Load.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour
+{
+    private void Start()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/General/Manager.cs
@@ -0,0 +1,182 @@
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using GameAnalyticsSDK;
+//using Facebook.Unity;
+
+public class Manager : MonoBehaviour
+{
+    // Singleton
+
+    private static Manager instance;
+    public static Manager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.Find("Manager").GetComponent<Manager>();
+                DontDestroyOnLoad(instance.gameObject);
+            }
+
+            return instance;
+        }
+    }
+
+
+    // Player
+
+    [HideInInspector]
+    public PlayerData PlayerData;
+
+
+    // Game Data
+
+
+
+    // Levels
+
+
+
+    // Data Handling
+
+    private JsonData jsonData;
+    private string dataPath;
+
+
+    // Unity Functions
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+            Initialize();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDisable()
+    {
+        Save();
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        /*
+        if (!pause)
+        {
+            if (FB.IsInitialized)
+            {
+                FB.ActivateApp();
+            }
+            else
+            {
+                FB.Init(() => { FB.ActivateApp(); });
+            }
+        }
+        */
+    }
+
+
+    // Functions
+
+    private void Initialize()
+    {
+        InitializeSDK();
+
+        InitializePlayerData();
+
+        // TO DO -> Initialize other data here.
+    }
+
+    private void InitializeSDK()
+    {
+        /*
+        GameAnalytics.Initialize();
+
+        if (FB.IsInitialized)
+        {
+            FB.ActivateApp();
+        }
+        else
+        {
+            FB.Init(() => { FB.ActivateApp(); });
+        }
+        */
+    }
+
+    private void InitializeUpgrades()
+    {
+        // TO DO -> Initialize PlayerData here.
+    }
+
+    public void Save()
+    {
+        SerializeData();
+    }
+
+
+
+    #region Data Handling
+
+    private void InitializePlayerData()
+    {
+        PlayerData = new PlayerData();
+        jsonData = new JsonData();
+
+        dataPath = Path.Combine(Application.persistentDataPath, "HyperSurvivorSave.json");
+
+        if (File.Exists(dataPath))
+        {
+            DeserializeData();
+        }
+        else
+        {
+            PlayerData.Coins = 0;
+
+            File.Create(dataPath).Close();
+
+            SerializeData();
+        }
+    }
+
+    // Saves progress data.
+    private void SerializeData()
+    {
+        jsonData.Coins = PlayerData.Coins;
+
+        string jsonDataString = JsonUtility.ToJson(jsonData, true);
+
+        File.WriteAllText(dataPath, jsonDataString);
+    }
+
+    // Loads progress data.
+    private void DeserializeData()
+    {
+        string jsonDataString = File.ReadAllText(dataPath);
+
+        jsonData = JsonUtility.FromJson<JsonData>(jsonDataString);
+
+        PlayerData.Coins = jsonData.Coins;
+    }
+
+    #endregion
+}
+
+public class JsonData
+{
+    public int Coins;
+
+    public JsonData()
+    {
+        Coins = 0;
+    }
+}
